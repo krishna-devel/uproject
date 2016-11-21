2123a0d971f8364ccadfded677d3f39169a4a24d
kkamath <kkamath@twitter.com>
2016-11-20 17:07:26 -0800

    Modified files

--- kdtree/src/MatrixXdAdapter.h
+++ kdtree/src/MatrixXdAdapter.h
@@ -1,43 +1,50 @@
 #ifndef KDTREE_MATRIXXDADAPTER_H
 #define KDTREE_MATRIXXDADAPTER_H
 
+#include <vector>
+#include <numeric>
 #include "eigen3/Eigen/Dense"
 
 using namespace std;
 using namespace Eigen;
-using Eigen::MatrixXd;
 using Eigen::Matrix;
 
 template <typename T>
 class MatrixXdAdapter {
 
 public:
+    // constructors
     MatrixXdAdapter(const Matrix<T, Dynamic, Dynamic> &matrix);
-//    MatrixXdAdapter(const double &matrix);
+    MatrixXdAdapter(const Matrix<T, Dynamic, Dynamic> &matrix, const vector<int> &indices);
 
-    int rows();
+    // getters
+    const Matrix<T, Dynamic, Dynamic> &getSamples() const { return samples; };
+    const vector<int> &getIndices() const { return indices; };
+
+    //
+//    const vector<T> &getValuesAlongAxis(int axis);
 
 private:
-//    double r;
-    Matrix<T, Dynamic, Dynamic> matrix;
-//    MatrixXd matrix;
+    Matrix<T, Dynamic, Dynamic> samples;
+    vector<int> indices;
 };
 
-
-//template <typename T>
-//int MatrixXdAdapter<T>::rows() {
-//    return 2;
-//}
-//
-//template <typename T>
-//MatrixXdAdapter<T>::MatrixXdAdapter(const double &r) : r(r) {}
-
 template <typename T>
-int MatrixXdAdapter<T>::rows() {
-    return this->matrix.rows();
+MatrixXdAdapter<T>::MatrixXdAdapter(const Matrix<T, Dynamic, Dynamic> &matrix) : samples(matrix) {
+    this->indices = vector<int>(matrix.rows());
+    iota(begin(this->indices), end(this->indices), 0);
 }
 
 template <typename T>
-MatrixXdAdapter<T>::MatrixXdAdapter(const Matrix<T, Dynamic, Dynamic> &matrix) : matrix(matrix) {}
+MatrixXdAdapter<T>::MatrixXdAdapter(
+        const Matrix<T, Dynamic, Dynamic> &matrix,
+        const vector<int> &indices
+) : samples(matrix), indices(indices) {}
+
+//template <typename T>
+//const vector<T>& MatrixXdAdapter<T>::getValuesAlongAxis(int axis) {
+//    return samples.col(axis);
+//}
+
 
 #endif //KDTREE_MATRIXXDADAPTER_H
--- kdtree/tests/basic_tests/MatrixXdAdapterTest.cpp
+++ kdtree/tests/basic_tests/MatrixXdAdapterTest.cpp
@@ -5,15 +5,33 @@
 class MatrixXdAdapterTest : public testing::Test {
 protected:
     virtual void SetUp() {
-        fMatrix << 1.f,2.f,3.f,4.f;
-        dMatrix << 1.0,2.0,3.0,4.0;
+        fMatrix <<
+                1.f,2.f,
+                3.f,4.f;
+        dMatrix <<
+                1.0,2.0,
+                3.0,4.0,
+                5.0,6.0;
     }
 
     Matrix<float, Dynamic, Dynamic> fMatrix {2, 2};
-    Matrix<double, Dynamic, Dynamic> dMatrix {2, 2};
+    Matrix<double, Dynamic, Dynamic> dMatrix {3, 2};
+
 };
 
-TEST_F(MatrixXdAdapterTest, check_rows) {
-    ASSERT_EQ(2, fMatrix.rows());
-    ASSERT_EQ(2, dMatrix.rows());
+TEST_F(MatrixXdAdapterTest, check_indices_are_correct_if_initialized_without_constructor) {
+    MatrixXdAdapter<float> adapter (fMatrix);
+    vector<int> expectedIndices {0,1};
+
+    ASSERT_EQ(expectedIndices, adapter.getIndices());
+    ASSERT_EQ(fMatrix.rows(), adapter.getIndices().size());
+}
+
+TEST_F(MatrixXdAdapterTest, check_indices_are_correct_if_initialized_with_constructor) {
+    vector<int> passedIndices {0,2};
+    MatrixXdAdapter<double> adapter (dMatrix, passedIndices);
+
+    vector<int> expectedIndices {0,2};
+    ASSERT_EQ(expectedIndices, adapter.getIndices());
+    ASSERT_EQ(fMatrix.rows(), adapter.getIndices().size());
 }
