5c3ddf4445c0412fc4e19e9cedb30763b6f38cc4
kkamath <kkamath@twitter.com>
2016-11-25 16:43:23 -0800

    Modified files

--- kdtree/main.cpp
+++ kdtree/main.cpp
@@ -1,59 +1,92 @@
 #include <iostream>
-
-#include <vector>
+#include <cstdlib>
+#include <pthread.h>
 
 using namespace std;
 
-//class A {
-//private:
-//    int a;
-//public:
-//    A(int a) : a(a) {}
-//};
-//
-//template <typename T>
-//class B : public A {
-//public:
-//    B(int a, T v1) : A(a), v1(v1) {}
-//    T getV1() const { return v1; }
-//private:
-//    T v1;
-//};
-//
-//template <typename U>
-//class C : public A {
-//private:
-//    U v2;
-//public:
-//    C(int a, U v2) : A(a), v2(v2) {}
-//    U getV2() const { return v2; }
-//};
+#define NUM_THREADS     5
 
-int main() {
-
-//    vector<A> v;
-//    v.push_back(new C<int>(10, 10));
-
-    cout << "Hello world!!" << endl;
-    cout << "0 " << (0-1)/2 << endl;
-    cout << "1 " << (1-1)/2 << endl;
-    cout << "2 " << (2-1)/2 << endl;
-    cout << "3 " << (3-1)/2 << endl;
-    cout << "4 " << (4-1)/2 << endl;
-    cout << "5 " << (5-1)/2 << endl;
-    cout << "6 " << (6-1)/2 << endl;
-
-//
-//    vector<int> v;
-//    v.reserve(10);
-//
-//    v[5l] = 15;
-//
-//    cout << "Hello world!! 4 " << v[4] << endl;
-//    cout << "Hello world!! 5 " << v[5] << endl;
-//
-//    for (int i: v) {
-//        cout << "v: " << i << endl;
-//    }
-    return 0;
+void *PrintHello(void *threadid) {
+    long tid;
+    tid = (intptr_t) threadid;
+    cout << "Hello World! Thread ID, " << tid << endl;
+    pthread_exit(NULL);
 }
+
+int main () {
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    int i;
+
+    for( i=0; i < NUM_THREADS; i++ ){
+        cout << "main() : creating thread, " << i << endl;
+        rc = pthread_create(&threads[i], NULL, PrintHello, (void *)(intptr_t)i);
+
+        if (rc){
+            cout << "Error:unable to create thread," << rc << endl;
+            exit(-1);
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+//#include <iostream>
+//
+//#include <vector>
+//
+//using namespace std;
+//
+////class A {
+////private:
+////    int a;
+////public:
+////    A(int a) : a(a) {}
+////};
+////
+////template <typename T>
+////class B : public A {
+////public:
+////    B(int a, T v1) : A(a), v1(v1) {}
+////    T getV1() const { return v1; }
+////private:
+////    T v1;
+////};
+////
+////template <typename U>
+////class C : public A {
+////private:
+////    U v2;
+////public:
+////    C(int a, U v2) : A(a), v2(v2) {}
+////    U getV2() const { return v2; }
+////};
+//
+//int main() {
+//
+////    vector<A> v;
+////    v.push_back(new C<int>(10, 10));
+//
+//    cout << "Hello world!!" << endl;
+//    cout << "0 " << (0-1)/2 << endl;
+//    cout << "1 " << (1-1)/2 << endl;
+//    cout << "2 " << (2-1)/2 << endl;
+//    cout << "3 " << (3-1)/2 << endl;
+//    cout << "4 " << (4-1)/2 << endl;
+//    cout << "5 " << (5-1)/2 << endl;
+//    cout << "6 " << (6-1)/2 << endl;
+//
+////
+////    vector<int> v;
+////    v.reserve(10);
+////
+////    v[5l] = 15;
+////
+////    cout << "Hello world!! 4 " << v[4] << endl;
+////    cout << "Hello world!! 5 " << v[5] << endl;
+////
+////    for (int i: v) {
+////        cout << "v: " << i << endl;
+////    }
+//    return 0;
+//}
