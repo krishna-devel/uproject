90a42658a1667d6eb4f6b3278d964e093d9fb1ab
kkamath <kkamath@twitter.com>
2016-12-05 10:01:16 -0800

    Modified files

--- kdtree/src/KDTreeExplorer.h
+++ kdtree/src/KDTreeExplorer.h
@@ -5,6 +5,7 @@
 #include "KDTree.h"
 #include "tbb/tbb.h"
 #include "tbb/parallel_do.h"
+#include <stack>
 
 using namespace std;
 using namespace tbb;
@@ -45,6 +46,35 @@ public:
 private:
     unique_ptr<Point<DataType, DimensionType>> point;
     const DataType distance;
+};
+
+template <typename DataType, typename DimensionType>
+class NearestNeighbor1 {
+public:
+    NearestNeighbor1(
+        const Point<DataType, DimensionType> &point,
+        const DataType distance) : point(point), distance(distance) {}
+    const Point<DataType, DimensionType> &getPoint() const { return point; }
+    const DataType getDistance() const { return distance; }
+private:
+    Point<DataType, DimensionType> point;
+    const DataType distance;
+};
+
+template <typename DataType, typename DimensionType>
+class NodeToExplore {
+public:
+    NodeToExplore(
+        const DimensionType nodeId,
+        const Node<DataType, DimensionType> *node,
+        const DataType distanceFromNode) : nodeId(nodeId), node(node), distanceFromNode(distanceFromNode) {}
+    const DimensionType getNodeId() const { return nodeId; }
+    const Node<DataType, DimensionType> *getNode() const { return node; }
+    const DataType getDistanceFromNode() const { return distanceFromNode; }
+private:
+    const DimensionType nodeId;
+    const Node<DataType, DimensionType> *node;
+    const DataType distanceFromNode;
 };
 
 template <typename DataType, typename DimensionType>
@@ -137,6 +167,76 @@ public:
         parallel_do( list.begin(), list.end(), ParallelNodeExplorer<DataType, DimensionType>() );
         return getNearestNeighbor(samples, query, potentialNeighbors);
     }
+
+
+    static NodeToExplore<DataType, DimensionType> *getNodeToExplore(
+        const Segment<DataType, DimensionType> &segment,
+        const DimensionType nodeId,
+        Node<DataType, DimensionType> *node,
+        const Point<DataType, DimensionType> &query
+    ) {
+        DataType distance = 0;
+        if (node->getType() == NodeType::INTERNAL) {
+            Bounds<DataType, DimensionType> nodeBounds(
+                node->getSplit()->getLeftBounds().getMinPoint().getCoefficients(),
+                node->getSplit()->getLeftBounds().getMaxPoint().getCoefficients()
+            );
+            distance = nodeBounds.distanceFromPoint(query);
+        } else {
+            distance = Point<DataType, DimensionType>::squaredDistance(segment.getPoint(node->getSampleId()), query);
+        }
+        return new NodeToExplore<DataType, DimensionType>(nodeId, node, distance);
+    }
+
+    static NearestNeighbor1<DataType, DimensionType> *findNearestNeighbor1(
+        const Samples<DataType> &samples,
+        const KDTree<DataType, DimensionType> &kdTree,
+        const Point<DataType, DimensionType> &query
+    ) {
+        Segment<DataType, DimensionType> segment(samples);
+
+        NearestNeighbor1<DataType, DimensionType> *currentNearestNeighbor =
+            new NearestNeighbor1<DataType, DimensionType>(segment.getPoint(0), numeric_limits<double>::infinity());
+
+        stack<NodeToExplore<DataType, DimensionType>*> nodesToExplore;
+        nodesToExplore.push(getNodeToExplore(segment, 0, kdTree.getNode(0), query));
+
+        while (!nodesToExplore.empty()) {
+            NodeToExplore<DataType, DimensionType> *nodeToExplore = nodesToExplore.top();
+            nodesToExplore.pop();
+
+            const Node<DataType, DimensionType> *node = nodeToExplore->getNode();
+            const DimensionType nodeId = nodeToExplore->getNodeId();
+
+            if (node->getType() == NodeType::LEAF) {
+                Point<DataType, DimensionType> point = segment.getPoint(node->getSampleId());
+                const DataType distanceFromNodeToExplore = nodeToExplore->getDistanceFromNode();
+//                DataType distanceFromNode = Point<DataType, DimensionType>::squaredDistance(point, query);
+                if (distanceFromNodeToExplore < currentNearestNeighbor->getDistance()) {
+                    currentNearestNeighbor =
+                        new NearestNeighbor1<DataType, DimensionType>(point, distanceFromNodeToExplore);
+                }
+            } else {
+                const DimensionType leftNodeId = kdTree.leftNodeId(nodeId);
+                NodeToExplore<DataType, DimensionType> *leftNodeToExplore =
+                    getNodeToExplore(segment, leftNodeId, kdTree.getNode(leftNodeId), query);
+
+                const DimensionType rightNodeId = kdTree.rightNodeId(nodeId);
+                NodeToExplore<DataType, DimensionType> *rightNodeToExplore =
+                    getNodeToExplore(segment, rightNodeId, kdTree.getNode(rightNodeId), query);
+
+                if (leftNodeToExplore->getDistanceFromNode() <= currentNearestNeighbor->getDistance()) {
+                    nodesToExplore.push(leftNodeToExplore);
+                }
+                if (rightNodeToExplore->getDistanceFromNode() <= currentNearestNeighbor->getDistance()) {
+                    nodesToExplore.push(rightNodeToExplore);
+                }
+            }
+        }
+
+        return currentNearestNeighbor;
+    };
+
 };
 
 #endif //KDTREE_KDTREEEXPLORER_H
