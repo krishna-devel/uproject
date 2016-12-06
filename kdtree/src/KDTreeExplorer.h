fcd2b1ef2e5ec552823235e01f487ca7bd64f1ce
kkamath <kkamath@twitter.com>
2016-12-05 20:03:09 -0800

    Modified files

--- kdtree/src/KDTreeExplorer.h
+++ kdtree/src/KDTreeExplorer.h
@@ -3,62 +3,23 @@
 
 #include "memory"
 #include "KDTree.h"
-#include "tbb/tbb.h"
-#include "tbb/parallel_do.h"
 #include <stack>
+#include <math.h>
 
 using namespace std;
-using namespace tbb;
-
-template <typename DataType, typename DimensionType>
-class DataToSearchNodes {
-public:
-    DataToSearchNodes(
-        const KDTree<DataType, DimensionType> &kdtree,
-        const Point<DataType, DimensionType> &query,
-        const DimensionType nodeId,
-        concurrent_vector<DimensionType> *potentialNeighbors
-    ) : kdtree(kdtree), query(query), nodeId(nodeId), potentialNeighbors(potentialNeighbors) {}
-    const KDTree<DataType, DimensionType> &getKdtree() const { return kdtree; }
-    const Point<DataType, DimensionType> &getQuery() const { return query; }
-    const DimensionType getNodeId() const { return nodeId; }
-    concurrent_vector<DimensionType, cache_aligned_allocator<DimensionType>> *getPotentialNeighbors() const {
-        return potentialNeighbors;
-    }
-private:
-    const KDTree<DataType, DimensionType> &kdtree;
-    const Point<DataType, DimensionType> &query;
-    const DimensionType nodeId;
-    concurrent_vector<DimensionType> *potentialNeighbors;
-};
 
 template <typename DataType, typename DimensionType>
 class NearestNeighbor {
 public:
     NearestNeighbor(
-        const vector<DataType> &coefficients,
-        const DataType distance
-    ) : distance(distance) {
-        point = unique_ptr<Point<DataType, DimensionType>> (new Point<DataType, DimensionType>(coefficients));
-    }
-    const unique_ptr<Point<DataType, DimensionType>> &getPoint() { return point; }
-    const DataType getDistance() const { return distance; }
-private:
-    unique_ptr<Point<DataType, DimensionType>> point;
-    const DataType distance;
-};
-
-template <typename DataType, typename DimensionType>
-class NearestNeighbor1 {
-public:
-    NearestNeighbor1(
         const Point<DataType, DimensionType> &point,
-        const DataType distance) : point(point), distance(distance) {}
+        const DataType distance) : point(point), squaredDistance(distance) {}
     const Point<DataType, DimensionType> &getPoint() const { return point; }
-    const DataType getDistance() const { return distance; }
+    const DataType getSquaredDistance() const { return squaredDistance; }
+    const DataType getEuclideanDistance() const { return sqrt(squaredDistance); }
 private:
     Point<DataType, DimensionType> point;
-    const DataType distance;
+    const DataType squaredDistance;
 };
 
 template <typename DataType, typename DimensionType>
@@ -79,96 +40,7 @@ private:
 
 template <typename DataType, typename DimensionType>
 class ParallelNodeExplorer {
-public:
-    vector<DimensionType> getNodesToSearch(const DataToSearchNodes<DataType, DimensionType> &dataToSearchNodes) const {
-        vector<DimensionType> nodesToExplore;
-
-        const KDTree<DataType, DimensionType> &kdTree = dataToSearchNodes.getKdtree();
-        const DimensionType nodeId = dataToSearchNodes.getNodeId();
-        const Point<DataType, DimensionType> &query = dataToSearchNodes.getQuery();
-        concurrent_vector<DimensionType> *potentialNeighbors = dataToSearchNodes.getPotentialNeighbors();
-        Node<DataType, DimensionType> *node = kdTree.getNode(nodeId);
-
-        if (node->getType() == NodeType::LEAF) {
-            potentialNeighbors->push_back(node->getSampleId());
-        } else {
-            // Make decision based on split threshold
-            DimensionType splitDimension = node->getSplit()->getSplitDimension();
-            DataType splitThreshold = node->getSplit()->getSplitThreshold();
-            bool shouldMoveLeftBySplit = query.getCoefficients()[splitDimension] < splitThreshold;
-
-            // Make decision based on bounds
-            DataType distanceFromLeft = node->getSplit()->getLeftBounds().distanceFromPoint(query);
-            DataType distanceFromRight = node->getSplit()->getRightBounds().distanceFromPoint(query);
-
-            if (shouldMoveLeftBySplit) {
-                nodesToExplore.push_back(kdTree.leftNodeId(nodeId));
-            } else {
-                nodesToExplore.push_back(kdTree.rightNodeId(nodeId));
-            }
-
-            if (shouldMoveLeftBySplit && distanceFromRight <= distanceFromLeft) {
-                nodesToExplore.push_back(kdTree.rightNodeId(nodeId));
-            } else if (!shouldMoveLeftBySplit && distanceFromRight >= distanceFromLeft) {
-                nodesToExplore.push_back(kdTree.leftNodeId(nodeId));
-            }
-        }
-        return nodesToExplore;
-    }
-
-    void operator()(
-        const DataToSearchNodes<DataType, DimensionType> &dataToSearchNodes,
-        parallel_do_feeder<DataToSearchNodes<DataType, DimensionType>> &feeder
-    ) const {
-        for (DimensionType nodeId: getNodesToSearch(dataToSearchNodes)) {
-            DataToSearchNodes<DataType, DimensionType> nextDataToSearchNodes(
-                dataToSearchNodes.getKdtree(),
-                dataToSearchNodes.getQuery(),
-                nodeId,
-                dataToSearchNodes.getPotentialNeighbors()
-            );
-            feeder.add(nextDataToSearchNodes);
-        }
-    }
-
-    static NearestNeighbor<DataType, DimensionType> *getNearestNeighbor(
-        const Samples<DataType> &samples,
-        const Point<DataType, DimensionType> &query,
-        concurrent_vector<DimensionType> potentialNeighbors
-    ) {
-        vector<DimensionType> sampleIdsInSegment;
-        for (DimensionType id : potentialNeighbors) {
-            sampleIdsInSegment.push_back(id);
-        }
-        Segment<DataType, DimensionType> segment (samples, sampleIdsInSegment);
-
-        Point<DataType, DimensionType> currentNearestNeighbor = segment.getPoint(sampleIdsInSegment[0]);
-        DataType currentLowestDistance = numeric_limits<double>::infinity();
-        for (DimensionType id: sampleIdsInSegment) {
-            Point<DataType, DimensionType> neighbor = segment.getPoint(id);
-            DataType distance = Point<DataType, DimensionType>::euclideanDistance(query, neighbor);
-            if (distance < currentLowestDistance) {
-                currentNearestNeighbor = neighbor;
-                currentLowestDistance = distance;
-            }
-        }
-
-        return new NearestNeighbor<DataType, DimensionType>(currentNearestNeighbor.getCoefficients(), currentLowestDistance);
-    }
-
-    static NearestNeighbor<DataType, DimensionType> *findNearestNeighbor(
-        const Samples<DataType> &samples,
-        const KDTree<DataType, DimensionType> &kdTree,
-        const Point<DataType, DimensionType> &query
-    ) {
-        concurrent_vector<DimensionType> potentialNeighbors;
-        DataToSearchNodes<DataType, DimensionType> dataToSearchNodes (kdTree, query, 0, &potentialNeighbors);
-        vector<DataToSearchNodes<DataType, DimensionType>> list {dataToSearchNodes};
-        parallel_do( list.begin(), list.end(), ParallelNodeExplorer<DataType, DimensionType>() );
-        return getNearestNeighbor(samples, query, potentialNeighbors);
-    }
-
-
+private:
     static NodeToExplore<DataType, DimensionType> *getNodeToExplore(
         const Segment<DataType, DimensionType> &segment,
         const DimensionType nodeId,
@@ -187,16 +59,16 @@ public:
         }
         return new NodeToExplore<DataType, DimensionType>(nodeId, node, distance);
     }
-
-    static NearestNeighbor1<DataType, DimensionType> *findNearestNeighbor1(
+public:
+    static NearestNeighbor<DataType, DimensionType> *findNearestNeighbor(
         const Samples<DataType> &samples,
         const KDTree<DataType, DimensionType> &kdTree,
         const Point<DataType, DimensionType> &query
     ) {
         Segment<DataType, DimensionType> segment(samples);
 
-        NearestNeighbor1<DataType, DimensionType> *currentNearestNeighbor =
-            new NearestNeighbor1<DataType, DimensionType>(segment.getPoint(0), numeric_limits<double>::infinity());
+        NearestNeighbor<DataType, DimensionType> *currentNearestNeighbor =
+            new NearestNeighbor<DataType, DimensionType>(segment.getPoint(0), numeric_limits<double>::infinity());
 
         stack<NodeToExplore<DataType, DimensionType>*> nodesToExplore;
         nodesToExplore.push(getNodeToExplore(segment, 0, kdTree.getNode(0), query));
@@ -211,10 +83,9 @@ public:
             if (node->getType() == NodeType::LEAF) {
                 Point<DataType, DimensionType> point = segment.getPoint(node->getSampleId());
                 const DataType distanceFromNodeToExplore = nodeToExplore->getDistanceFromNode();
-//                DataType distanceFromNode = Point<DataType, DimensionType>::squaredDistance(point, query);
-                if (distanceFromNodeToExplore < currentNearestNeighbor->getDistance()) {
+                if (distanceFromNodeToExplore < currentNearestNeighbor->getSquaredDistance()) {
                     currentNearestNeighbor =
-                        new NearestNeighbor1<DataType, DimensionType>(point, distanceFromNodeToExplore);
+                        new NearestNeighbor<DataType, DimensionType>(point, distanceFromNodeToExplore);
                 }
             } else {
                 const DimensionType leftNodeId = kdTree.leftNodeId(nodeId);
@@ -225,10 +96,10 @@ public:
                 NodeToExplore<DataType, DimensionType> *rightNodeToExplore =
                     getNodeToExplore(segment, rightNodeId, kdTree.getNode(rightNodeId), query);
 
-                if (leftNodeToExplore->getDistanceFromNode() <= currentNearestNeighbor->getDistance()) {
+                if (leftNodeToExplore->getDistanceFromNode() <= currentNearestNeighbor->getSquaredDistance()) {
                     nodesToExplore.push(leftNodeToExplore);
                 }
-                if (rightNodeToExplore->getDistanceFromNode() <= currentNearestNeighbor->getDistance()) {
+                if (rightNodeToExplore->getDistanceFromNode() <= currentNearestNeighbor->getSquaredDistance()) {
                     nodesToExplore.push(rightNodeToExplore);
                 }
             }
--- kdtree/tests/basic_tests/KDTreeExplorerTest.cpp
+++ kdtree/tests/basic_tests/KDTreeExplorerTest.cpp
@@ -1,103 +1,103 @@
-#include <gmock/gmock.h>
-#include "KDTreeExplorer.h"
-
-TEST(ParallelNodeSearcherTest, test_potential_neigbors_is_filled_when_node_is_leaf) {
-
-    KDTree<float, int> kdTree(5);
-    kdTree.insertLeafNode(2, 2);
-    kdTree.insertLeafNode(3, 0);
-    kdTree.insertLeafNode(4, 1);
-
-    vector<float> query {1,2};
-    concurrent_vector<int> potentialNeighbors;
-
-    ParallelNodeExplorer<float, int> parallelNodeExplorer;
-    parallelNodeExplorer.getNodesToSearch(DataToSearchNodes<float, int>(kdTree, query, 2, &potentialNeighbors));
-    parallelNodeExplorer.getNodesToSearch(DataToSearchNodes<float, int>(kdTree, query, 3, &potentialNeighbors));
-    parallelNodeExplorer.getNodesToSearch(DataToSearchNodes<float, int>(kdTree, query, 4, &potentialNeighbors));
-    ASSERT_EQ(2, potentialNeighbors[0]);
-    ASSERT_EQ(0, potentialNeighbors[1]);
-    ASSERT_EQ(1, potentialNeighbors[2]);
-
-}
-
-
-TEST(ParallelNodeSearcherTest, test_potential_neigbors_is_filled_when_node_is_internal) {
-
-    KDTree<float, int> kdTree(5);
-
-    vector<float> maxLeft {-1,2};
-    vector<float> minLeft {-3,0};
-    vector<float> maxRight {3,2};
-    vector<float> minRight {1,0};
-    vector<float> splitPoint {0,0};
-
-    Split<float, int> split(
-        0,
-        splitPoint,
-        Bounds<float, int>(maxLeft, minLeft),
-        Bounds<float, int>(maxRight, minRight)
-    );
-
-    kdTree.insertInternalNode(1, split);
-    kdTree.insertLeafNode(3, 3);
-    kdTree.insertLeafNode(4, 4);
-
-    vector<float> pointInsideLeftBounds {-2,1};
-    vector<float> pointInsideRightBounds {-4,4};
-    vector<float> pointOutsideLeftBoundsOnLeft {2,1};
-    vector<float> pointOutsideRightBoundsOnRight {4,4};
-    vector<float> pointEquidistantFromBounds {0, 4};
-
-    concurrent_vector<int> potentialNeighbors;
-
-    ParallelNodeExplorer<float, int> parallelNodeExplorer;
-    vector<int> nextNeighbors1 = parallelNodeExplorer
-        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointInsideLeftBounds, 1, &potentialNeighbors));
-    ASSERT_EQ(1, nextNeighbors1.size());
-    ASSERT_EQ(3, nextNeighbors1[0]);
-
-    vector<int> nextNeighbors2 = parallelNodeExplorer
-        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointInsideRightBounds, 1, &potentialNeighbors));
-    ASSERT_EQ(1, nextNeighbors2.size());
-    ASSERT_EQ(3, nextNeighbors2[0]);
-
-    vector<int> nextNeighbors3 = parallelNodeExplorer
-        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointOutsideLeftBoundsOnLeft, 1, &potentialNeighbors));
-    ASSERT_EQ(1, nextNeighbors3.size());
-    ASSERT_EQ(4, nextNeighbors3[0]);
-
-    vector<int> nextNeighbors4 = parallelNodeExplorer
-        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointOutsideRightBoundsOnRight, 1, &potentialNeighbors));
-    ASSERT_EQ(1, nextNeighbors4.size());
-    ASSERT_EQ(4, nextNeighbors4[0]);
-
-    vector<int> nextNeighbors5 = parallelNodeExplorer
-        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointEquidistantFromBounds, 1, &potentialNeighbors));
-    ASSERT_EQ(2, nextNeighbors5.size());
-    ASSERT_EQ(4, nextNeighbors5[0]);
-    ASSERT_EQ(3, nextNeighbors5[1]);
-
-}
-
-TEST(ParallelNodeSearcherTest, test_getNearestNeighbor) {
-    vector<float> query {2,1};
-
-    Samples<float> samples {4,2};
-    samples << 100,101,
-            -100,101,
-            1,2,
-            200,200;
-
-    concurrent_vector<int> potentialNeighbors;
-    potentialNeighbors.push_back(0);
-    potentialNeighbors.push_back(2);
-    potentialNeighbors.push_back(3);
-
-    NearestNeighbor<float, int> *nearestNeighbor =
-        ParallelNodeExplorer<float, int>::getNearestNeighbor(samples, query, potentialNeighbors);
-
-    vector<float> expectedNearestNeighbor {1,2};
-    EXPECT_EQ(expectedNearestNeighbor, nearestNeighbor->getPoint()->getCoefficients());
-    EXPECT_NEAR(1.414, nearestNeighbor->getDistance(), 0.001);
-}
+//#include <gmock/gmock.h>
+//#include "KDTreeExplorer.h"
+//
+//TEST(ParallelNodeSearcherTest, test_potential_neigbors_is_filled_when_node_is_leaf) {
+//
+//    KDTree<float, int> kdTree(5);
+//    kdTree.insertLeafNode(2, 2);
+//    kdTree.insertLeafNode(3, 0);
+//    kdTree.insertLeafNode(4, 1);
+//
+//    vector<float> query {1,2};
+//    concurrent_vector<int> potentialNeighbors;
+//
+//    ParallelNodeExplorer<float, int> parallelNodeExplorer;
+//    parallelNodeExplorer.getNodesToSearch(DataToSearchNodes<float, int>(kdTree, query, 2, &potentialNeighbors));
+//    parallelNodeExplorer.getNodesToSearch(DataToSearchNodes<float, int>(kdTree, query, 3, &potentialNeighbors));
+//    parallelNodeExplorer.getNodesToSearch(DataToSearchNodes<float, int>(kdTree, query, 4, &potentialNeighbors));
+//    ASSERT_EQ(2, potentialNeighbors[0]);
+//    ASSERT_EQ(0, potentialNeighbors[1]);
+//    ASSERT_EQ(1, potentialNeighbors[2]);
+//
+//}
+//
+//
+//TEST(ParallelNodeSearcherTest, test_potential_neigbors_is_filled_when_node_is_internal) {
+//
+//    KDTree<float, int> kdTree(5);
+//
+//    vector<float> maxLeft {-1,2};
+//    vector<float> minLeft {-3,0};
+//    vector<float> maxRight {3,2};
+//    vector<float> minRight {1,0};
+//    vector<float> splitPoint {0,0};
+//
+//    Split<float, int> split(
+//        0,
+//        splitPoint,
+//        Bounds<float, int>(maxLeft, minLeft),
+//        Bounds<float, int>(maxRight, minRight)
+//    );
+//
+//    kdTree.insertInternalNode(1, split);
+//    kdTree.insertLeafNode(3, 3);
+//    kdTree.insertLeafNode(4, 4);
+//
+//    vector<float> pointInsideLeftBounds {-2,1};
+//    vector<float> pointInsideRightBounds {-4,4};
+//    vector<float> pointOutsideLeftBoundsOnLeft {2,1};
+//    vector<float> pointOutsideRightBoundsOnRight {4,4};
+//    vector<float> pointEquidistantFromBounds {0, 4};
+//
+//    concurrent_vector<int> potentialNeighbors;
+//
+//    ParallelNodeExplorer<float, int> parallelNodeExplorer;
+//    vector<int> nextNeighbors1 = parallelNodeExplorer
+//        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointInsideLeftBounds, 1, &potentialNeighbors));
+//    ASSERT_EQ(1, nextNeighbors1.size());
+//    ASSERT_EQ(3, nextNeighbors1[0]);
+//
+//    vector<int> nextNeighbors2 = parallelNodeExplorer
+//        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointInsideRightBounds, 1, &potentialNeighbors));
+//    ASSERT_EQ(1, nextNeighbors2.size());
+//    ASSERT_EQ(3, nextNeighbors2[0]);
+//
+//    vector<int> nextNeighbors3 = parallelNodeExplorer
+//        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointOutsideLeftBoundsOnLeft, 1, &potentialNeighbors));
+//    ASSERT_EQ(1, nextNeighbors3.size());
+//    ASSERT_EQ(4, nextNeighbors3[0]);
+//
+//    vector<int> nextNeighbors4 = parallelNodeExplorer
+//        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointOutsideRightBoundsOnRight, 1, &potentialNeighbors));
+//    ASSERT_EQ(1, nextNeighbors4.size());
+//    ASSERT_EQ(4, nextNeighbors4[0]);
+//
+//    vector<int> nextNeighbors5 = parallelNodeExplorer
+//        .getNodesToSearch(DataToSearchNodes<float, int>(kdTree, pointEquidistantFromBounds, 1, &potentialNeighbors));
+//    ASSERT_EQ(2, nextNeighbors5.size());
+//    ASSERT_EQ(4, nextNeighbors5[0]);
+//    ASSERT_EQ(3, nextNeighbors5[1]);
+//
+//}
+//
+//TEST(ParallelNodeSearcherTest, test_getNearestNeighbor) {
+//    vector<float> query {2,1};
+//
+//    Samples<float> samples {4,2};
+//    samples << 100,101,
+//            -100,101,
+//            1,2,
+//            200,200;
+//
+//    concurrent_vector<int> potentialNeighbors;
+//    potentialNeighbors.push_back(0);
+//    potentialNeighbors.push_back(2);
+//    potentialNeighbors.push_back(3);
+//
+//    NearestNeighbor<float, int> *nearestNeighbor =
+//        ParallelNodeExplorer<float, int>::getNearestNeighbor(samples, query, potentialNeighbors);
+//
+//    vector<float> expectedNearestNeighbor {1,2};
+//    EXPECT_EQ(expectedNearestNeighbor, nearestNeighbor->getPoint()->getCoefficients());
+//    EXPECT_NEAR(1.414, nearestNeighbor->getDistance(), 0.001);
+//}
