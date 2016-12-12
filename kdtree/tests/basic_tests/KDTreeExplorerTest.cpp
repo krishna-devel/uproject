525549c0bec78700622ca91c82ce573e29d86c45
kkamath <kkamath@twitter.com>
2016-12-12 12:31:31 -0800

    Modified files

--- kdtree/tests/basic_tests/KDTreeExplorerTest.cpp
+++ kdtree/tests/basic_tests/KDTreeExplorerTest.cpp
@@ -8,7 +8,7 @@ TEST(KDTreeExplorerTest, test_query_and_kd_tree_dimensions_dont_match) {
             Samples<float> samples {2, 2};
             samples << 1,2,3,4;
             KDTree<float, int> kdTree (10);
-            vector<float> queryWithWrongDimension {1,3};
+            vector<float> queryWithWrongDimension {1,3,4};
             KDTreeExplorer<float, int>::findNearestNeighbor(samples, kdTree, queryWithWrongDimension);
         }),
         query_and_kd_tree_dimensions_dont_match
