1fe3719e73e5fb909430eba2463d8c69af0293c8
kkamath <kkamath@twitter.com>
2016-12-12 12:28:45 -0800

    Modified files

--- kdtree/tests/basic_tests/KDTreeExplorerTest.cpp
+++ kdtree/tests/basic_tests/KDTreeExplorerTest.cpp
@@ -1,19 +1,14 @@
 #include <gmock/gmock.h>
 #include "KDTreeExplorer.h"
 
-
-
-
-
 TEST(KDTreeExplorerTest, test_query_and_kd_tree_dimensions_dont_match) {
 
-
-
-ASSERT_THROW(({
+    ASSERT_THROW(
+        ({
             Samples<float> samples {2, 2};
             samples << 1,2,3,4;
             KDTree<float, int> kdTree (10);
-            vector<float> queryWithWrongDimension {1,3,4};
+            vector<float> queryWithWrongDimension {1,3};
             KDTreeExplorer<float, int>::findNearestNeighbor(samples, kdTree, queryWithWrongDimension);
         }),
         query_and_kd_tree_dimensions_dont_match
